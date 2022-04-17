b448348b27748c7efe6fefd592b215af39785825
FlameyFox <flameblader95@live.dk>
2022-04-17 18:41:46 +0200

    changed to nuxt2 and reworked supabase

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({ $supabase }) {
+    return {
+      //events: await $supabase.from('movies').select('*'),
+    }
+  },
+}
+</script>
