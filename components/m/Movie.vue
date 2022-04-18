cabdf59dca46a973b1446c2514657f10b2cbde90
FlameyFox <flameblader95@live.dk>
2022-04-18 13:19:07 +0200

    mhm

--- components/m/Movie.vue
+++ components/m/Movie.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="...">
-    {{ movie }}
     <h3>{{ movie.original_title }}</h3>
     <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.original_title" />
+    <!-- TODO: Add fallback image -->
     <p>{{ movie.overview }}</p>
   </div>
 </template>
--- components/m/TopMovies.vue
+++ components/m/TopMovies.vue
@@ -2,7 +2,7 @@
   <div class="max-w-screen-2xl m-auto">
     <h2>Top Movies</h2>
     <div v-if="loading">Loading...</div>
-    <div v-else class="grid grid-cols-6 gap-4">
+    <div v-else class="grid grid-cols-6 gap-6">
       <MMovie :movie="movie" v-for="movie in movies" :key="movie.id" />
     </div>
   </div>
@@ -21,7 +21,6 @@ export default {
     const url =
       'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=' +
       api
-    console.log(url)
     const movies = await this.$axios.$get(url)
     this.movies = movies.results
     this.loading = false
