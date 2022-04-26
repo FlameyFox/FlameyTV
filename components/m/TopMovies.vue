8aac4c08037b7b96d0e7e9713f10cb71919a6ec6
FlameyFox <flameblader95@live.dk>
2022-04-26 22:54:18 +0200

    yip

--- components/m/TopMovies.vue
+++ components/m/TopMovies.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="max-w-screen-2xl m-auto py-6">
-      <h2 class="text-4xl font-black">Top Movies</h2>
-      <div v-if="loading">
-        <div class="grid grid-cols-6 gap-6">
-          <div
-            class="bg-slate-700 shadow rounded-md p-4 max-w-sm w-full mx-auto animate-pulse"
-            v-for="num in 6"
-            :key="num"
-          ></div>
-        </div>
+  <div class="max-w-screen-2xl m-auto py-6">
+    <h2 class="text-4xl font-black">Top Movies</h2>
+    <div v-if="loading">
+      <div class="grid grid-cols-6 gap-6">
+        <div
+          class="bg-slate-700 shadow rounded-md p-4 max-w-sm w-full mx-auto animate-pulse"
+          v-for="num in 6"
+          :key="num"
+        ></div>
       </div>
-      <div v-else>
-        <div v-if="!showMore" class="grid grid-cols-6 gap-6">
-          <MMovie
-            :movie="movie"
-            :loading="loading"
-            v-for="movie in movies.slice(0, 6)"
-            :key="movie.id"
-          />
-        </div>
-        <div v-else class="grid grid-cols-6 gap-6">
-          <MMovie
-            :movie="movie"
-            :loading="loading"
-            v-for="movie in movies.slice(0, 18)"
-            :key="movie.id"
-          />
-        </div>
-      </div>
-      <button
-        v-if="!showMore"
-        class="p-2 rounded-md text-center bg-slate-900 mt-6 mx-auto block"
-        @click="showMore = true"
-      >
-        Show more
-      </button>
     </div>
+    <div v-else>
+      <div v-if="!showMore" class="grid grid-cols-6 gap-6">
+        <MMovie
+          :movie="movie"
+          :loading="loading"
+          v-for="movie in movies.slice(0, 6)"
+          :key="movie.id"
+        />
+      </div>
+      <div v-else class="grid grid-cols-6 gap-6">
+        <MMovie
+          :movie="movie"
+          :loading="loading"
+          v-for="movie in movies.slice(0, 18)"
+          :key="movie.id"
+        />
+      </div>
+    </div>
+    <button
+      v-if="!showMore"
+      class="p-2 rounded-md text-center bg-slate-900 mt-6 mx-auto block"
+      @click="showMore = true"
+    >
+      Show more
+    </button>
+  </div>
 </template>
 <script>
 const api = process.env.TMDB_API
