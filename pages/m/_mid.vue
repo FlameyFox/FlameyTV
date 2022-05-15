cb385ef9f431d6ff44f1a61f0bab0ecb5095bbde
FlameyFox <flameblader95@live.dk>
2022-05-15 22:30:42 +0200

    tweaks and tests

--- pages/m/_mid.vue
+++ pages/m/_mid.vue
@@ -3,18 +3,37 @@
     <div v-if="loading">Loading...</div>
     <div v-else>
       <img
-        v-if="movie.poster_path"
-        :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+        v-if="movie.backdrop_path"
+        :src="
+          'https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/' +
+          movie.backdrop_path
+        "
         :alt="movie.title"
         class="w-60"
       />
-      <img
-        class="bg-white w-60"
-        v-else
-        src="@/assets/img/noPoster.png"
-        alt="No Poster"
-      />
       <h1>{{ movie.title }}</h1>
+      <div class="details">
+        <img
+          v-if="movie.poster_path"
+          :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+          :alt="movie.title"
+          class="w-60"
+        />
+        <img
+          class="bg-white w-60"
+          v-else
+          src="@/assets/img/noPoster.png"
+          alt="No Poster"
+        />
+        <p>Rating: {{ movie.vote_average }}</p>
+        <p>Budget: {{ movie.budget }}</p>
+        <p>Revenue: {{ movie.revenue }}</p>
+        <p>IMDB id: {{ movie.imdb_id }}</p>
+        <p>Description: {{ movie.overview }}</p>
+
+
+        <!-- TODO: Get actor info -->
+      </div>
     </div>
   </div>
 </template>
