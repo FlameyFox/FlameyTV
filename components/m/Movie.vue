9da61bcb0038f14555797fe2aacceb5dbb24380a
FlameyFox <flameblader95@live.dk>
2022-06-19 18:17:49 +0200

    tv shows

--- components/m/Movie.vue
+++ components/m/Movie.vue
@@ -1,5 +1,59 @@
 <template>
-  <div class="...">
+  <div v-if="movie.media_type" class="...">
+    <div v-if="movie.media_type === 'movie'" class="movie overflow-hidden">
+      <nuxt-link :to="'/m/' + movie.id" class="block relative">
+        <img
+          v-if="movie.poster_path"
+          :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+          :alt="movie.title"
+        />
+        <img
+          class="bg-white"
+          v-else
+          src="@/assets/img/noPoster.png"
+          alt="No Poster"
+        />
+        <div
+          class="details w-full flex h-full items-end justify-between p-2 absolute bottom-0"
+        >
+          <aside>
+            <h3>{{ movie.title }}</h3>
+            {{ movie.release_date }}
+          </aside>
+          <aside class="rounded-md p-1 text-center movie-score">
+            {{ movie.vote_average ? movie.vote_average.toFixed(1) : '0' }}
+          </aside>
+        </div>
+      </nuxt-link>
+    </div>
+    <div v-if="movie.media_type === 'tv'" class="movie overflow-hidden">
+      <nuxt-link :to="'/tv/' + movie.id" class="block relative">
+        <img
+          v-if="movie.poster_path"
+          :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+          :alt="movie.title"
+        />
+        <img
+          class="bg-white"
+          v-else
+          src="@/assets/img/noPoster.png"
+          alt="No Poster"
+        />
+        <div
+          class="details w-full flex h-full items-end justify-between p-2 absolute bottom-0"
+        >
+          <aside>
+            <h3>{{ movie.name }}</h3>
+            {{ movie.first_air_date }}
+          </aside>
+          <aside class="rounded-md p-1 text-center movie-score">
+            {{ movie.vote_average ? movie.vote_average.toFixed(1) : 'N/A' }}
+          </aside>
+        </div>
+      </nuxt-link>
+    </div>
+  </div>
+  <div v-else class="...">
     <div class="movie overflow-hidden">
       <nuxt-link :to="'/m/' + movie.id" class="block relative">
         <img
---
+++ pages/tv/_tvid.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <div class="tv bg-no-repeat" :style="backdropImgPath">
+      <div class="details p-8">
+          <img
+            v-if="tv.poster_path"
+            :src="'https://image.tmdb.org/t/p/w500/' + tv.poster_path"
+            :alt="tv.name"
+            class="w-60"
+          />
+          <img
+            class="bg-white w-60"
+            v-else
+            src="@/assets/img/noPoster.png"
+            alt="No Poster"
+          />
+          <h1>{{ tv.name }}</h1>
+          <p>Rating: {{ tv.vote_average }}</p>
+          <p>Budget: {{ tv.budget }}</p>
+          <p>Revenue: {{ tv.revenue }}</p>
+          <p>IMDB id: {{ tv.imdb_id }}</p>
+          <p>Description: {{ tv.overview }}</p>
+          <p>Cast: {{ mCredits }}</p>
+
+          <!-- <div class="flex flex-wrap">
+            <span
+              v-for="member in cast"
+              :key="member.id"
+              class="inline-block mx-1"
+              ><nuxt-link :to="'/actor/' + member.id">{{
+                member.original_name
+              }}</nuxt-link>
+              as {{ member.character }},</span
+            >
+          </div> -->
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const api = process.env.TMDB_API
+export default {
+  data() {
+    return {
+      tv: null,
+      loading: false,
+      showMore: false,
+      backdropImgPath: {
+        backgroundImage: '',
+      },
+      cast: [],
+    }
+  },
+  async created() {
+    this.loading = true
+    const url =
+      'https://api.themoviedb.org/3/tv/' +
+      this.$route.params.tvid +
+      '?api_key=' +
+      api
+    const result = await this.$axios.$get(url)
+    this.tv = result
+    this.backdropImgPath.backgroundImage =
+      'url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/' +
+      result.backdrop_path +
+      ')'
+
+    this.getMovieCredits(this.$route.params.mid)
+    this.loading = false
+  },
+
+  methods: {
+    async getMovieCredits(ID) {
+      this.loading = true
+      const url =
+        'https://api.themoviedb.org/3/tv/' + ID + '/credits?api_key=' + api
+      const credits = await this.$axios.$get(url)
+      this.cast = credits.cast
+      this.loading = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.details {
+  background-color: rgba(0, 0, 0, 0.8);
+}
+</style>
