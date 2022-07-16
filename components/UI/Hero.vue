a0f19c1b380ae54036260b8b5b945ff3256c4575
FlameyFox <flameblader95@live.dk>
2022-07-16 23:34:47 +0200

    tweaked hero and added keep-alive to template

--- components/UI/Hero.vue
+++ components/UI/Hero.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
     <section class="h-96 bg-slate-900 bg-opacity-60 overflow-hidden">
-      <div class="pt-1 pl-1 opacity-60" v-if="loading">Loading</div>
-      <div class="h-full relative" v-else-if="movies">
-        <img
-          :src="
-            'https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/' +
-            movies[this.number].backdrop_path
-          "
-          alt="movie.title"
-          class="opacity-40 h-full w-full object-cover"
-        />
+      <div class="pt-1 pl-1 opacity-20" v-if="loading">Loading</div>
+      <div
+        class="h-full hero relative bg-cover bg-no-repeat bg-center"
+        :style="backdropImgPath"
+        v-else-if="movies"
+      >
         <nuxt-link
           class="absolute top-1 left-1 opacity-60 hover:opacity-100 transition-all"
           :to="'/m/' + movies[this.number].id"
@@ -29,6 +25,9 @@ export default {
       movies: null,
       loading: false,
       number: 0,
+      backdropImgPath: {
+        backgroundImage: '',
+      },
     }
   },
   async created() {
@@ -36,8 +35,25 @@ export default {
     const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + api
     const movies = await this.$axios.$get(url)
     this.movies = movies.results
-    this.number = Math.floor(Math.random() * 20) + 1
+    this.number = Math.floor(Math.random() * 20)
+
+    this.backdropImgPath.backgroundImage =
+      'url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/' +
+      this.movies[this.number].backdrop_path +
+      ')'
     this.loading = false
   },
 }
 </script>
+
+<style scoped>
+.hero:before {
+  content: '';
+  background-color: rgba(0, 0, 0, 0.8);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <UITheHeader />
     <section class="bg-slate-800 min-h-screen text-slate-200">
-      <Nuxt />
+      <Nuxt keep-alive />
     </section>
     <UITheFooter />
   </div>
