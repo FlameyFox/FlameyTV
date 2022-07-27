1a99e8e597d5ceb55aa7042629107dd896f010a4
FlameyFox <flameblader95@live.dk>
2022-07-27 19:30:38 +0200

    tweaked footer

--- components/UI/TheFooter.vue
+++ components/UI/TheFooter.vue
@@ -2,11 +2,12 @@
   <div>
     <footer class="bg-slate-900 text-slate-200 text-sm flex p-4">
       <p>
-        FlameyTV - Portfolio project using TMDB -
+        FlameyTV - Portfolio project using TMDB - Work in progress -
         <a
           target="_blank"
           rel="noopener nofollow"
           href="https://github.com/FlameyFox/FlameyTV"
+          class="hover:underline transition-all"
           >Link to project</a
         >
       </p>
